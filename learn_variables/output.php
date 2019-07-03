98e8ef1d825ec69cc605c3b30898ef0a264b34b0
mehwishliaqat <50008375+mehwishliaqat@users.noreply.github.com>
2019-07-03 10:17:54 -0700

    Add files via upload

---
+++ learn_variables/output.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+    
+<?php
+$txt = "Web Develpers";
+echo "I Love $txt!";
+?>
+    
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "W3Schools.com";
+echo "I love " . $txt . "!";
+?>
+
+</body>
+</html>
